e6aa17b807c3ed67fd691bee8b77dfeddeb85e0c
Navin Mohan <navinmohan81@gmail.com>
2017-04-28 01:24:19 +0530

    gui added

---
+++ gui/menu/main.cpp
@@ -0,0 +1,11 @@
+#include "menu.h"
+#include <QDesktopWidget>
+#include <QApplication>
+int main(int argc, char *argv[])
+{
+QApplication app(argc, argv);
+MyMenu window;
+window.setWindowTitle("My menu");
+window.show();
+return app.exec();
+}
